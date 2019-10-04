9003c170588dd6679aec4f987e68072ded81df70
JiEun Hong <39687573+ParticleChef@users.noreply.github.com>
2019-10-04 21:39:40 +0900

    Update copyResult_hls.sh

--- copyResult_hls.sh
+++ copyResult_hls.sh
@@ -10,5 +10,5 @@ cp MET* Result_files/${1}/codes/.
 cp met/solution_test/syn/report/* Result_files/${1}/reports
 cp met/solution_test/csim/build/*.txt Result_files/${1}/txtfiles
 
-#scp -r Result_files/${1} jieun@155.230.154.54:/home/jieun/MET_hls/parti10000/.
+
 
